63aa5d60d12f0de1581eb0f60b119ab0dbf17dd5
Dao Thi Truc Loan <your_email@gmail.com>
2026-03-02 12:52:01 +0000

    Add UNet and BTXRD dataset loader

---
+++ src/unet.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+class UNet(nn.Module):
+    def __init__(self, in_ch=1, out_ch=1, base=32):
+        super().__init__()
+        self.d1 = DoubleConv(in_ch, base)
+        self.p1 = nn.MaxPool2d(2)
+        self.d2 = DoubleConv(base, base*2)
+        self.p2 = nn.MaxPool2d(2)
+        self.d3 = DoubleConv(base*2, base*4)
+        self.p3 = nn.MaxPool2d(2)
+        self.d4 = DoubleConv(base*4, base*8)
+
+        self.u3 = nn.ConvTranspose2d(base*8, base*4, 2, stride=2)
+        self.c3 = DoubleConv(base*8, base*4)
+        self.u2 = nn.ConvTranspose2d(base*4, base*2, 2, stride=2)
+        self.c2 = DoubleConv(base*4, base*2)
+        self.u1 = nn.ConvTranspose2d(base*2, base, 2, stride=2)
+        self.c1 = DoubleConv(base*2, base)
+
+        self.out = nn.Conv2d(base, out_ch, 1)
+
+    def forward(self, x):
+        x1 = self.d1(x)
+        x2 = self.d2(self.p1(x1))
+        x3 = self.d3(self.p2(x2))
+        x4 = self.d4(self.p3(x3))
+
+        y = self.u3(x4)
+        y = self.c3(torch.cat([y, x3], dim=1))
+        y = self.u2(y)
+        y = self.c2(torch.cat([y, x2], dim=1))
+        y = self.u1(y)
+        y = self.c1(torch.cat([y, x1], dim=1))
+        return self.out(y)
